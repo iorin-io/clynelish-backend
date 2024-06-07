8afa6d62e92536f3a84ba643b6cf83ae478bb48c
iorin-io <iodere77@gmail.com>
2024-06-07 17:53:18 +0900

    記述を簡潔に

--- src/routes.rs
+++ src/routes.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::get, routing::post, routing::put, routing::delete};
+use axum::{Router, routing::get, routing::post, routing::put};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use crate::db::AppState;
@@ -13,29 +13,19 @@ use crate::handlers::{
 
 pub fn create_routes(state: Arc<Mutex<AppState>>) -> Router {
     Router::new()
-        .route("/users", post(create_user))
-        .route("/users", get(get_users))
-        .route("/users/:id", get(get_user))
-        .route("/users/:id", put(update_user))
-        .route("/users/:id", delete(delete_user))
+        .route("/", get(|| async { "Hello, world!" }).post(|| async { "Hello, world!" }))
+        .route("/users", post(create_user).get(get_users))
+        .route("/users/:id", get(get_user).put(update_user).delete(delete_user))
         .route("/accounts", post(create_account))
-        .route("/accounts/:id", get(get_account))
-        .route("/accounts/:id", put(update_account))
-        .route("/accounts/:id", delete(delete_account))
+        .route("/accounts/:id", get(get_account).put(update_account).delete(delete_account))
         .route("/categories/parent", post(create_parent_category))
         .route("/categories/child", post(create_child_category))
         .route("/categories/:id", get(get_categories))
-        .route("/categories/parent/:id", put(update_parent_category))
-        .route("/categories/child/:id", put(update_child_category))
-        .route("/categories/parent/:id", delete(delete_parent_category))
-        .route("/categories/child/:id", delete(delete_child_category))
+        .route("/categories/parent/:id", put(update_parent_category).delete(delete_parent_category))
+        .route("/categories/child/:id", put(update_child_category).delete(delete_child_category))
         .route("/transactions", post(create_transaction))
-        .route("/transactions/:id", get(get_transaction))
-        .route("/transactions/:id", put(update_transaction))
-        .route("/transactions/:id", delete(delete_transaction))
+        .route("/transactions/:id", get(get_transaction).put(update_transaction).delete(delete_transaction))
         .route("/budgets", post(create_budget))
-        .route("/budgets/:id", get(get_budget))
-        .route("/budgets/:id", put(update_budget))
-        .route("/budgets/:id", delete(delete_budget))
+        .route("/budgets/:id", get(get_budget).put(update_budget).delete(delete_budget))
         .layer(axum::Extension(state))
 }
