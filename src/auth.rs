10c42b99e12ef1e8ed47db951b4b48dd2fb56af7
iorin-io <iodere77@gmail.com>
2024-06-12 14:18:27 +0900

    検証関数を追加

---
+++ src/auth.rs
@@ -0,0 +1,27 @@
+use jsonwebtoken::{decode, DecodingKey, Validation, Algorithm};
+use serde::Deserialize;
+use std::error::Error;
+use reqwest;
+
+#[derive(Deserialize)]
+struct FirebaseClaims {
+    email: String,
+    // 他の必要なクレームがあればここに追加
+}
+
+async fn verify_firebase_token(token: &str) -> Result<FirebaseClaims, Box<dyn Error>> {
+    let google_jwks_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/publicKeys";
+    let jwks: serde_json::Value = reqwest::get(google_jwks_url).await?.json().await?;
+
+    let header = decode::<serde_json::Value>(token, &DecodingKey::from_secret("your_secret".as_ref()), &Validation::new(Algorithm::RS256)).unwrap().header;
+    let kid = header.kid.ok_or("Invalid token: missing kid")?;
+
+    let jwk = jwks.get(&kid).ok_or("Invalid token: unknown kid")?;
+    let public_key = jwk["n"].as_str().ok_or("Invalid token: missing public key")?;
+
+    let decoding_key = DecodingKey::from_rsa_components(public_key, jwk["e"].as_str().unwrap())?;
+
+    let token_data = decode::<FirebaseClaims>(token, &decoding_key, &Validation::new(Algorithm::RS256))?;
+
+    Ok(token_data.claims)
+}
